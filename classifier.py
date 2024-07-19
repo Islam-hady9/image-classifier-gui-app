60a478535f48b53c32678f36fa97af95ba45231f
Islam Abd_Elhady <eslamabdo71239@gmail.com>
2024-07-20 01:31:29 +0300

    add full code

--- classifier.py
+++ classifier.py
@@ -1,26 +1,67 @@
 from taipy.gui import Gui
+from tensorflow.keras import models
+from PIL import Image
+import numpy as np
+
+class_names = {
+    0: 'airplane',
+    1: 'automobile',
+    2: 'bird',
+    3: 'cat',
+    4: 'deer',
+    5: 'dog',
+    6: 'frog',
+    7: 'horse',
+    8: 'ship',
+    9: 'truck',
+}
+
+model = models.load_model(r"model\baseline.keras")
+
+
+def predict_image(model, path_to_img):
+    img = Image.open(path_to_img)
+    img = img.convert("RGB")
+    img = img.resize((32, 32))
+    data = np.asarray(img)
+    data = data / 255
+    probs = model.predict(np.array([data])[:1])
+
+    top_prob = probs.max()
+    top_pred = class_names[np.argmax(probs)]
+
+    return top_prob, top_pred
+
 
 content = ""
 img_path = "img/placeholder_image.png"
+prob = 0
+pred = ""
 
 index = """
 <|text-center|
 <|{"img/logo.png"}|image|width=25vw|>
 
 <|{content}|file_selector|extensions=.png|>
-<|{content}|file_selector|extensions=.png|>
 select an image from your file system
+
+<|{pred}|>
 
 <|{img_path}|image|>
 
-<|label goes here|indicator|value=0|min=0|max=100|width=25vw|>
+<|{prob}|indicator|value={prob}|min=0|max=100|width=25vw|>
 >
 """
 
+
 def on_change(state, var_name, var_val):
     if var_name == "content":
+        top_prob, top_pred = predict_image(model, var_val)
+        state.prob = round(top_prob * 100)
+        state.pred = "this is a " + top_pred
         state.img_path = var_val
-    #print(var_name, var_val)
+    # print(var_name, var_val)
+
 
 app = Gui(page=index)
 
